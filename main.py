7f0b15bdd82176a8f3406ca273c6680899304fcc
rkran <rkranjith03@gmail.com>
2022-09-18 04:05:10 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from functools import wraps
 from sqlalchemy import Table, Column, Integer, ForeignKey
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 login_manager = LoginManager()
 login_manager.init_app(app)
 ckeditor = CKEditor(app)
@@ -31,7 +32,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL","sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
